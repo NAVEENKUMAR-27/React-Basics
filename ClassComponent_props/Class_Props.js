f9302f18939285b5277e610a2f40d656d6bbf620
NAVEENKUMAR-27 <122080230+NAVEENKUMAR-27@users.noreply.github.com>
2023-08-04 18:51:09 +0530

    class Component use props

---
+++ ClassComponent_props/Class_Props.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+
+export class Comments extends Component(){
+    render(){
+        return(
+            <h1>Hello{this.props.name}</h1>
+        )
+    }
+}
