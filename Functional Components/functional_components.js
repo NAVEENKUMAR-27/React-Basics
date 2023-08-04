4c330531e85707cab2586d89baeb7e018b06ef39
NAVEENKUMAR-27 <122080230+NAVEENKUMAR-27@users.noreply.github.com>
2023-08-04 18:52:10 +0530

    Functional Component

---
+++ Functional Components/functional_components.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css"
+
+        // how to use functional component 
+export function FunComponent(){
+    return(<>
+    <div class="card-deck">
+        <div class="card">
+            <img src="https://images.hindustantimes.com/img/2022/07/06/1600x900/dhoni-2016-getty_1654515327472_1657137509048.jpg" class="card-img-top" alt="..."/>
+            <div class="card-body">
+            <h5 class="card-title">MS DHONI </h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="https://images.hindustantimes.com/img/2022/07/06/1600x900/dhoni-2016-getty_1654515327472_1657137509048.jpg" class="card-img-top" alt="..."/>
+            <div class="card-body">
+            <h5 class="card-title">MS DHONI </h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="https://images.hindustantimes.com/img/2022/07/06/1600x900/dhoni-2016-getty_1654515327472_1657137509048.jpg" class="card-img-top" alt="..."/>
+            <div class="card-body">
+            <h5 class="card-title">MS DHONI </h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+                
+        
+    
+    </>)
+}
