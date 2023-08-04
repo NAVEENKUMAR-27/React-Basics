d33391886a3f7d8628b7c00db770f54754d9d8f7
NAVEENKUMAR-27 <122080230+NAVEENKUMAR-27@users.noreply.github.com>
2023-08-04 18:53:02 +0530

    Use multi component Props

---
+++ Multiple_comp_props/multi_props.js
@@ -0,0 +1,52 @@
+import React,{useState} from "react";
+
+export function First({props}){
+
+    return(
+        <>
+            <h1>{props}</h1>
+
+        </>
+    )
+    
+}
+export function Second({props}){
+
+    return(
+        <>
+            <h1>{props}</h1>
+
+        </>
+    )
+    
+}
+export function Third({props}){
+
+    return(
+        <>
+            <h1>{props}</h1>
+
+        </>
+    )
+    
+}
+export function FOURTH({props}){
+
+    return(
+        <>
+            <h1>{props}</h1>
+
+        </>
+    )
+    
+}
+export function Fifth({props}){
+
+    return(
+        <>
+            <h1>{props}</h1>
+
+        </>
+    )
+    
+}
