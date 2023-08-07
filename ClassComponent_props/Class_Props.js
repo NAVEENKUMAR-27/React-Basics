03c6aafa569ea5ad39ea1515ad97a78d3e4f2b21
NAVEENKUMAR-27 <122080230+NAVEENKUMAR-27@users.noreply.github.com>
2023-08-07 18:40:48 +0530

    class Component props

--- ClassComponent_props/Class_Props.js
+++ ClassComponent_props/Class_Props.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css"
 
 
-export class Comments extends Component(){
+export default class Classprops extends Component{
     render(){
-        return(
-            <h1>Hello{this.props.name}</h1>
+        
+        return(<>
+        <div className="App mt-5 pt-5">
+            <h1 className="text-primary" >My Name : {this.props.name}</h1>
+            <h5>From :<span className="text-primary">{this.props.place}</span></h5>
+            <p className="text-danger ">{this.props.quotes}</p>
+        </div>
+
+            </>
         )
     }
 }
